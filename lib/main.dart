31ed83703149754185b02ae6061f88a93bd4f4bb
anhtuan141 <anhtuan1411994@gmail.com>
2023-10-16 21:37:59 +0700

    Create file gradient_container.dart and import gradient_container.dart in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_first_app/gradient_container.dart';
+
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
       ),
@@ -10,29 +12,3 @@ void main() {
   );
 }
 
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context){
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.deepPurple,
-            Colors.orangeAccent,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
-}
