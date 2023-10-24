da620ff500da4b4417be3e0e91d3f2f842b1d790
anhtuan141 <anhtuan1411994@gmail.com>
2023-10-24 21:47:19 +0700

    Create function rollDice and create ElevateButton

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -9,6 +9,10 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
+  void rollDice() {
+
+  }
+
   @override
   Widget build(context) {
     return Container(
@@ -20,7 +24,17 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-          child: Image.asset('assets/images/dice-2.png')
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/dice-2.png',
+                width: 200,
+              ),
+              ElevatedButton(onPressed: rollDice,
+                child: const Text('Roll Dice'),
+              )
+            ],
+          )
       ),
     );
   }
