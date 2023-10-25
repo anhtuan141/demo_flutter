9712fb5f04e3045f7afc874ac81ffa08a6616a0c
anhtuan141 <anhtuan1411994@gmail.com>
2023-10-25 21:27:16 +0700

    write logic void rollDice

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,28 +1,34 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
-  @override
 
+  @override
   State<DiceRoller> createState() {
-   return _DiceRollerState();
+    return _DiceRollerState();
   }
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-2.png';
+  var currentDiceRoll = 2;
 
   void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
   }
 
   @override
-  Widget build(context){
+  Widget build(context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
         const SizedBox(height: 20),
@@ -31,9 +37,7 @@ class _DiceRollerState extends State<DiceRoller> {
           style: ElevatedButton.styleFrom(
             foregroundColor: Colors.white,
             backgroundColor: Colors.orangeAccent,
-            textStyle: const TextStyle(
-                fontSize: 28
-            ),
+            textStyle: const TextStyle(fontSize: 28),
           ),
           child: const Text('Roll Dice'),
         )
